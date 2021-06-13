e2a7e58c1068aaf868770f07d4fbcc238ea086c3
Adrian <tamkeyeung.adrian@gmail.com>
2021-06-14 01:20:19 +0900

    update v1.0

--- src/image_processing.py
+++ src/image_processing.py
@@ -1,14 +1,9 @@
-import datetime
-import io, os
-from pathlib import Path
-from tkinter.constants import BROWSE
-
-import cv2
-import numpy as np
-from matplotlib import pyplot as plt
 import PySimpleGUI as sg
-
+import os, cv2, io
+from pathlib import Path
 import imutils
+import matplotlib.pyplot as plt
+import numpy as np
 
 def get_tree_data(parent, dirname):
     treedata = sg.TreeData()
@@ -49,155 +44,182 @@ def draw_plot(img_f):
 
     return item.getvalue()
 
-original_image = [[sg.Image(filename='', key='-orginal_img-')]]
-modify_image = [[sg.Image(filename='', key='-modify_img-')]]
 
-image_layout = [[sg.Frame(title="Original", layout=original_image), sg.Frame(title="Modify", layout=modify_image)]]
+def define_layout():
 
 
-threshold_radio = sg.Radio('Threshold', 'Radio', size=(10, 1), key='-THRESHOLD-')
-threshold_slid = sg.Slider((0, 255), 128, 1, orientation='h', size=(30, 15), key='-thslid-')
+    threshold_radio = sg.Radio('Threshold', 'Radio', size=(10, 1), key='-THRESHOLD-')
+    threshold_slid = sg.Slider((0, 255), 128, 1, orientation='h', size=(30, 15), key='-thslid-')
 
-blur_radio = sg.Radio('Blur', 'Radio', size=(10, 1), key='-BLUR-')
-blur_slid = sg.Slider((0, 11), 1, 1, orientation='h', size=(30, 15), key='-BLUR SLIDER-')
+    blur_radio = sg.Radio('Blur', 'Radio', size=(10, 1), key='-BLUR-')
+    blur_slid = sg.Slider((0, 11), 1, 1, orientation='h', size=(30, 15), key='-BLUR SLIDER-')
 
-hue_radio = sg.Radio('Hue', 'Radio', size=(10, 1), key='-HUE-')
-hue_slid = sg.Slider((0, 225), 0, 1, orientation='h', size=(30, 15), key='-HUE SLIDER-')
+    hue_radio = sg.Radio('Hue', 'Radio', size=(10, 1), key='-HUE-')
+    hue_slid = sg.Slider((0, 225), 0, 1, orientation='h', size=(30, 15), key='-HUE SLIDER-')
 
-ehance_radio =  sg.Radio('Ehance', 'Radio', size=(10, 1), key='-ENHANCE-')
-ehance_slid = sg.Slider((1, 225), 0, 1, orientation='h', size=(30, 15), key='-ENHANCE SLIDER-')
+    ehance_radio =  sg.Radio('Ehance', 'Radio', size=(10, 1), key='-ENHANCE-')
+    ehance_slid = sg.Slider((1, 225), 0, 1, orientation='h', size=(30, 15), key='-ENHANCE SLIDER-')
 
-canny_radio = sg.Radio('Canny', 'Radio', size=(10, 1), key='-CANNY-')
-canny_a = sg.Slider((0, 255), 128, 1, orientation='h', size=(15, 15), key='-CANNY SLIDER A-')
-canny_b = sg.Slider((0, 255), 128, 1, orientation='h', size=(15, 15), key='-CANNY SLIDER B-')
+    canny_radio = sg.Radio('Canny', 'Radio', size=(10, 1), key='-CANNY-')
+    canny_a = sg.Slider((0, 255), 128, 1, orientation='h', size=(15, 15), key='-CANNY SLIDER A-')
+    canny_b = sg.Slider((0, 255), 128, 1, orientation='h', size=(15, 15), key='-CANNY SLIDER B-')
 
-read_layout = [[sg.Text("  File:   "), sg.InputText(key='-input_file-', enable_events=True, ),
-                sg.FileBrowse('Browse', key='-file-', target="-input_file-",), sg.Button('Read', key='-read_file-')],
-                [sg.Text("Output:"), sg.InputText(key='-out_path-', enable_events=True,)],
-                [sg.Cancel(), sg.Button('Save',key='-save-',button_color=('black', '#4adcd6')), sg.ProgressBar(100, orientation='h', size=(20, 20),bar_color=("purple", "green"), key='-PROGRESS BAR-'), sg.Text("         ",key='-saved-')]]
+    denoise_radio = sg.Radio('Denoise', 'Radio', size=(10, 1), key='-DENOISE-')
+    denoise_level = sg.Slider((1, 20), 1, 1, orientation='h', size=(30, 15), key='-DENOISE LEVEL-')
 
-setting_layout =[
-                [threshold_radio, threshold_slid],
-                [blur_radio, blur_slid],
-                [hue_radio, hue_slid],
-                [ehance_radio, ehance_slid],
-                [canny_radio, canny_a, canny_b]
+    setting_layout =[
+                    [threshold_radio, threshold_slid],
+                    [hue_radio, hue_slid],
+                    [blur_radio, blur_slid],
+                    [denoise_radio, denoise_level],
+                    [ehance_radio, ehance_slid],
+                    [canny_radio, canny_a, canny_b]
+                    ]
+
+    read_layout = [[sg.Text("Folder: "), sg.InputText(key='-browse_folder-', enable_events=True, ),
+                    sg.FolderBrowse('Browse', key='-file-', target="-browse_folder-"), sg.Button('Read', key='-read_folder-')],
+                    [sg.Text("Output:"), sg.InputText(key='-out_path-', enable_events=True,)],
+                    [sg.Cancel(), sg.Button('Save',key='-save-',button_color=('black', '#4adcd6')), sg.ProgressBar(100, orientation='h', size=(20, 20),bar_color=("purple", "green"), key='-PROGRESS BAR-'), sg.Text("         ",key='-saved-')]]
+
+
+
+    original_image = [[sg.Image(filename='', key='-orginal_img-')]]
+    modify_image = [[sg.Image(filename='', key='-modify_img-')]]
+
+    layout_2 = [[sg.Frame("Read", read_layout)],
+                [sg.Frame("Setting", setting_layout)],
+                [sg.Frame(title="Original", layout=original_image),
+                sg.Frame(title="Modify", layout=modify_image)],
+                [sg.Output(size=(60,15), font='Courier 8')]
                 ]
-hist_graph = [[sg.Image(filename='', key='-hist_img-')]]
+
+    asthetic_layout = [[sg.T('Anything that you would use for asthetics is in this tab!')],
+                [sg.Image(data=sg.DEFAULT_BASE64_ICON,  k='-IMAGE-')],
+                [sg.ProgressBar(1000, orientation='h', size=(20, 20), key='-PROGRESS_BAR-'), sg.Button('Test Progress bar')]]
+
+    menu_def = [['&Application', ['E&xit']],
+                ['&About', ['&About']] ]
+
+    treedata = get_tree_data("", os.getcwd())
+
+    tree_layout=[[sg.Tree(
+        data=treedata,
+        headings=[],
+        auto_size_columns=True,
+        num_rows=45,
+        col0_width=30,
+        key="-TREE-",
+        show_expanded=False,
+        enable_events=True)]]
+
+    layout_1 = [[sg.Frame(title="", layout=tree_layout), sg.Frame(title=None, layout=layout_2)]]
+
+    layout = [[sg.Menu(menu_def, key='-MENU-')],
+                [sg.Text('', size=(38, 1), justification='center', font=("Helvetica", 16), relief=sg.RELIEF_RIDGE, k='-TEXT HEADING-', enable_events=True)]]
+    layout +=[[sg.TabGroup([[   sg.Tab('Image_Processing', layout_1),
+                                sg.Tab('Test', asthetic_layout)
+                                ]], key='-TAB GROUP-')]]
+    return layout
 
 
-treedata = get_tree_data("", os.getcwd())
-
-tree_layout=[[sg.Tree(
-    data=treedata,
-    headings=[],
-    auto_size_columns=True,
-    num_rows=12,
-    col0_width=30,
-    key="-TREE-",
-    show_expanded=False,
-    enable_events=True)]]
-
-log = [[sg.Output(size=(60,15), font='Courier 8')]]
-
-tree_summary = [[sg.Frame(title="", layout=tree_layout), sg.Frame(title="", layout=log)]]
-
-
-layout_img= [
-    [sg.Frame(title='Read',layout=read_layout)],
-    [sg.Frame(title='Parameter', layout=setting_layout)],
-    [sg.Frame(title="Image", layout=image_layout)]
-    ]
-
-
-
-browse_layout = [[sg.Text("Folder: "), sg.InputText(key='-browse_folder-', enable_events=True),
-                    sg.FolderBrowse('Browse', key='-folder-', target="-browse_folder-"), sg.Button('OK', key="-read_folder-")]]
-
-logging_layout = [  [sg.Frame(title="Folder", layout=browse_layout)],
-                    [sg.Frame(title="",layout = tree_summary)],
-                    [sg.Text("Anything printed will display here!")]]
-
-layout = [[sg.Text('Demo Of (Almost) All Elements', size=(38, 1), justification='center', font=("Helvetica", 16), relief=sg.RELIEF_RIDGE, k='-TEXT HEADING-', enable_events=True)]]
-layout +=[[sg.TabGroup([[   sg.Tab('Input Elements', layout_img),
-                            sg.Tab('Output', logging_layout)]], key='-TAB GROUP-')]]
-layout_tot = layout
-
-READ_File = False
-
-sg.theme('Dark Blue 3')
-
-window = sg.Window('Image Processing', layout_tot,
+def main():
+    READ_File = False
+   
+    layout = define_layout()
+    
+    window = sg.Window('Image_Processing_GUI', layout,
                    location=(10, 10),alpha_channel=1.0,
                    no_titlebar=False,grab_anywhere=False, element_justification="center").Finalize()
+    
+    while True:
+        event, values = window.read(timeout=100)
+        
+        if event in (None, 'Cancel', 'Exit'):
+            break
 
-while True:
+        elif event == "-read_folder-":
+            window["-TREE-"].update(values=get_tree_data("", values["-browse_folder-"]))
 
-    event, values = window.read(timeout=20)
+        elif event == "-TREE-":
+            read_path = Path(values['-TREE-'][0])
+            _, file_ext = os.path.splitext(read_path) 
 
-    if event in (None, 'Cancel'):
-        break
+            if file_ext in [".jpg", ".png"]:
+                print(f"[-LOG-] Chosen: {read_path.stem}")
+                READ_File = True
+                default_out = str(read_path).replace(str(read_path.stem),str(read_path.stem)+"_modified")
+                window['-out_path-'].Update(value=default_out)
+                
+                read_img = cv2.imread(str(read_path))
+                read_img = imutils.resize(read_img, width=400)
+                imgbytes = cv2.imencode('.png', read_img)[1].tobytes()
+                mod_img = read_img.copy()
+                window['-orginal_img-'].update(data=imgbytes)
+                window['-modify_img-'].update(data=imgbytes)
+            else:
+                print("[Error] Please select a file of type [jpg] or [png].")
 
-    elif event == "-read_folder-":
-        window["-TREE-"].update(values=get_tree_data("", values["-browse_folder-"]))
+        elif event == 'About':
+            print("[LOG] Clicked About!")
+            sg.popup(
+                '========================================',
+                'TITLE:\tA simply python-based image processing GUI.',
+                '========================================',                
+                'Author: Adrian Tam',
+                'Version: 1.0',
+                'Updated: 2021/06/12',
+                '========================================',                
+                'Instruction:',                
+                '========================================',                
+                'Choose an image from the file browser and change parameters to modify the image.',                
+                'Press "Save" to save the modified image.',                
+                )
 
-    elif event == "-TREE-":
-        print(values["-TREE-"])
+        if READ_File:    
+            if event == '-save-':
+                out_path = values['-out_path-']
+                for i in range(100):
+                    window['-PROGRESS BAR-'].UpdateBar(i + 1)
+                cv2.imwrite(out_path, mod_img)
+                window['-saved-'].update(value="Saved!")
+                
+            elif values['-THRESHOLD-'] :
+                _, mod_img  = cv2.threshold(read_img, int(values['-thslid-']), 255, cv2.THRESH_BINARY)
+                window['-thslid-'].Update(int(values['-thslid-']))
 
-    elif event == '-read_file-':
-        read_path = Path(values['-input_file-'])
-        default_out = str(read_path).replace(str(read_path.stem),str(read_path.stem)+"_modified")
-        window['-out_path-'].Update(value=default_out)
+            elif values['-BLUR-'] :
+                mod_img  = cv2.GaussianBlur(read_img, (21, 21), values['-BLUR SLIDER-'])
+                window['-BLUR SLIDER-'].Update(int(values['-BLUR SLIDER-']))
 
-        read_img = cv2.imread(str(read_path))
-        read_img = imutils.resize(read_img, width=400)
-        imgbytes = cv2.imencode('.png', read_img)[1].tobytes()
-        mod_img = read_img.copy()
-        histbytes = draw_plot(read_img)
-        window['-orginal_img-'].update(data=imgbytes)
-        window['-modify_img-'].update(data=imgbytes)
-        # window['-hist_img-'].update(data=histbytes)
-        READ_File = True
+            elif values['-DENOISE-']:
+                mod_img = cv2.fastNlMeansDenoisingColored(read_img, None, values["-DENOISE LEVEL-"],values["-DENOISE LEVEL-"],7,21)
 
-    if READ_File:    
-        if event == '-save-':
-            out_path = values['-out_path-']
-            for i in range(100):
-                window['-PROGRESS BAR-'].UpdateBar(i + 1)
-            cv2.imwrite(out_path, mod_img)
-            window['-saved-'].update(value="Saved!")
-            
-        elif values['-THRESHOLD-'] :
-            _, mod_img  = cv2.threshold(read_img, int(values['-thslid-']), 255, cv2.THRESH_BINARY)
-            window['-thslid-'].Update(int(values['-thslid-']))
+            elif values['-HUE-'] :
+                mod_img = cv2.cvtColor(read_img, cv2.COLOR_BGR2HSV)
+                mod_img[:, :, 0] += int(values['-HUE SLIDER-'])
+                mod_img = cv2.cvtColor(mod_img, cv2.COLOR_HSV2BGR)            
+                window['-HUE SLIDER-'].Update(int(values['-HUE SLIDER-']))
 
-        elif values['-BLUR-'] :
-            mod_img  = cv2.GaussianBlur(read_img, (21, 21), values['-BLUR SLIDER-'])
-            window['-BLUR SLIDER-'].Update(int(values['-BLUR SLIDER-']))
+            elif values['-ENHANCE-']:
+                enh_val = values['-ENHANCE SLIDER-'] / 40
+                clahe = cv2.createCLAHE(clipLimit=enh_val, tileGridSize=(8, 8))
+                lab = cv2.cvtColor(read_img, cv2.COLOR_BGR2LAB)
+                lab[:, :, 0] = clahe.apply(lab[:, :, 0])
+                mod_img = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
+                window['-HUE SLIDER-'].Update(int(values['-HUE SLIDER-']))
 
-        elif values['-HUE-'] :
-            mod_img = cv2.cvtColor(read_img, cv2.COLOR_BGR2HSV)
-            mod_img[:, :, 0] += int(values['-HUE SLIDER-'])
-            mod_img = cv2.cvtColor(mod_img, cv2.COLOR_HSV2BGR)            
-            window['-HUE SLIDER-'].Update(int(values['-HUE SLIDER-']))
+            elif values['-CANNY-']:
+                mod_img = cv2.Canny(read_img, values['-CANNY SLIDER A-'], values['-CANNY SLIDER B-'])
 
-        elif values['-ENHANCE-']:
-            enh_val = values['-ENHANCE SLIDER-'] / 40
-            clahe = cv2.createCLAHE(clipLimit=enh_val, tileGridSize=(8, 8))
-            lab = cv2.cvtColor(read_img, cv2.COLOR_BGR2LAB)
-            lab[:, :, 0] = clahe.apply(lab[:, :, 0])
-            mod_img = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
-            window['-HUE SLIDER-'].Update(int(values['-HUE SLIDER-']))
-
-        elif values['-CANNY-']:
-            mod_img = cv2.Canny(read_img, values['-CANNY SLIDER A-'], values['-CANNY SLIDER B-'])
-
-        # if not values['-CANNY-']:
-            # histbytes = draw_plot(mod_img)
-            # window['-hist_img-'].update(data=histbytes)
-
-        mod_imgbytes = cv2.imencode('.png', mod_img)[1].tobytes()
-        window['-modify_img-'].update(data=mod_imgbytes)
+            mod_imgbytes = cv2.imencode('.png', mod_img)[1].tobytes()
+            window['-modify_img-'].update(data=mod_imgbytes)
 
 
-window.close()
+
+
+        
+    window.close()
+    exit(0)
+
+if __name__ == '__main__':
+    main()
