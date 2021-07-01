47ad32908114c9cf502e24a8740f60e57acbe758
Adrian <tamkayeung.adrian@gmail.com>
2021-07-01 20:41:59 +0900

    testing new stuffs

--- src/new_scripts.py
+++ src/new_scripts.py
@@ -15,7 +15,8 @@ import scipy.cluster
 plt.style.use("ggplot")
 
 # img_path = r'D:\Python\git\Python_GUI\data\img\FLIR_09312.jpg'
-img_path = r'D:\Python\git\Python_GUI\data\img\test.png'
+img_path = r'C:\Users\ipx\Desktop\Personal\Python_GUI\data\img\FLIR_09312.jpg'
+# img_path = r'C:\Users\ipx\Desktop\Personal\Python_GUI\data\img\test.png'
 
 
 img = cv2.imread(img_path)
@@ -27,6 +28,54 @@ shape = ar.shape
 ar = ar.reshape(np.product(shape[:2]), shape[2]).astype(float)
 
 
+from sklearn.mixture import BayesianGaussianMixture
+
+bgm = BayesianGaussianMixture(n_components=20, random_state=1, weight_concentration_prior_type="dirichlet_process", init_params="kmeans")
+
+bgm.fit(ar)
+
+
+print(
+    "Number of clusters:\t",len(bgm.means_)
+)
+
+print(
+    bgm.weights_
+)
+
+
+
+
+
+
+
+
+vecs, dist = scipy.cluster.vq.vq(ar, bgm.means_)
+counts, bins = np.histogram(vecs, len(bgm.means_))
+
+
+
+for idx, (k,v) in enumerate(zip(bgm.means_,counts)):
+    
+    # plt.hist([idx]*counts[idx], color=(1,0,0))
+    plt.hist([idx]*round(100*v/(img.shape[0]*img.shape[1])), color=k/255, rwidth=19)
+
+
+
+print(set(vecs))
+
+
+
+
+
+# for i, (m, w) in enumerate(zip(bgm.means_, bgm.weights_)):
+    # print(m, w)
+
+plt.show()
+
+
+
+# exit()
 import pandas as pd
 
 
@@ -56,26 +105,19 @@ elbow_plot = pd.DataFrame({'num_clusters' : np.arange(1,20),
 
 import seaborn as sns
 sns.lineplot(x = 'num_clusters', y = 'distortions', data = elbow_plot)
-# plt.xticks(np.arange(1,6))
-plt.show()
+# plt.show()
+# plt.clf()
 
-plt.clf()
-
-# exit()
 print('reading image')
-# im = Image.open('image.jpg')
-# im = im.resize((150, 150))      # optional, to reduce time
 
-# im = cv2.resize(img, (150,150))
+# NUM_CLUSTERS = 20
 
-NUM_CLUSTERS = 15
+# print('finding clusters')
+# codes, dist = scipy.cluster.vq.kmeans(ar, NUM_CLUSTERS)
+# print('cluster centres:\n', codes)
 
-print('finding clusters')
-codes, dist = scipy.cluster.vq.kmeans(ar, NUM_CLUSTERS)
-print('cluster centres:\n', codes)
-
-vecs, dist = scipy.cluster.vq.vq(ar, codes)         # assign codes
-counts, bins = np.histogram(vecs, len(codes))    # count occurrences
+# vecs, dist = scipy.cluster.vq.vq(ar, codes)         # assign codes
+# counts, bins = np.histogram(vecs, len(codes))    # count occurrences
 
 
 for idx, (k,v) in enumerate(zip(codes,counts)):
@@ -85,12 +127,7 @@ for idx, (k,v) in enumerate(zip(codes,counts)):
 
 # N, bins, patches = plt.hist(vecs, bins =bins)
 
-# print(codes[0])
-
-# for idx, patch in enumerate(patches):
-    # patches[idx].set_facecolor()
-
-plt.show()
+# plt.show()
 
 
 index_max = np.argmax(counts)                    # find most frequent
@@ -98,10 +135,10 @@ peak = codes[index_max]
 colour = binascii.hexlify(bytearray(int(c) for c in peak)).decode('ascii')
 print('most frequent is %s (#%s)' % (peak, colour))
 
-# plt.imshow([colour])
-# plt.show()
+# plt.style.use("dark_background")
 
-plt.style.use("dark_background")
+
+codes = bgm.means_[np.where(bgm.weights_ > 0.001)]
 
 
 row = 3
@@ -109,7 +146,6 @@ col = len(codes)//3 + 1
 
 fig, ax = plt.subplots(row,col)
 
-# fig.axis("off")
 
 ax = ax.flatten()
 
@@ -118,12 +154,16 @@ ax = ax.flatten()
 import imageio
 c = ar.copy()
 
+
 for i, code in enumerate(codes):
+    
+    print(code)
+    
     d = np.ones_like(c)
     
     c[scipy.r_[np.where(vecs==i)],:] = code
     
-    d[scipy.r_[np.where(vecs==i)],:] = 255
+    d[scipy.r_[np.where(vecs==i)],:] = code
     
     
 
@@ -132,14 +172,7 @@ for i, code in enumerate(codes):
     ax[i].set_title(np.array(code, dtype=int))
     ax[i].imshow(tmp)
     
-        
-    # plt.imshow(tmp)
-
-# plt.imshow(c.reshape(*shape).astype(np.uint8))
-    
-    
-# imageio.imwrite('clusters.png', c.reshape(*shape).astype(np.uint8))
-# print('saved clustered image')
+fig.tight_layout()
 plt.show()
 
 
---
+++ src/test.py
@@ -0,0 +1,179 @@
+from cv2 import data
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+import scipy.cluster
+from sklearn.mixture import BayesianGaussianMixture
+
+
+"""
+    1. Read image
+    2. Preprocess image
+    3. Apply Gaussaian Mixture
+    4. Choose top 5 
+    5. Plot results
+
+
+"""
+
+
+
+def read_image(img_path):
+    img = cv2.imread(img_path)
+    # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    return img
+
+def preprocess(img, *params):
+    
+    img = cv2.resize(img, (128,128))
+    array = np.array(img)
+    shape = array.shape
+    array = array.reshape(np.product(shape[:2]), shape[2]).astype(float)
+
+    return array, shape
+
+def gaussian_mixture(array, *params):
+    bgm = BayesianGaussianMixture(
+        n_components=10, 
+        random_state=1, 
+        weight_concentration_prior_type="dirichlet_process", 
+        init_params="kmeans",
+        covariance_type="full")
+
+    bgm.fit(array)
+
+    vecs, dist = scipy.cluster.vq.vq(array, bgm.means_)
+    counts, bins = np.histogram(vecs, len(bgm.means_))
+    
+    return bgm.means_, bgm.weights_, counts, vecs
+
+def plot_histogram(centroids, counts):
+    new_center = centroids[np.where(counts>0)]
+    new_count = counts[np.where(counts>0)]
+    
+    row = len(new_count)
+    col = 1
+    
+    list1 = [x for x,_ in sorted(zip(new_center, new_count), key = lambda pair: pair[1], reverse=True)]
+    list2 = sorted(new_count, reverse=True)
+    
+    
+    fig, axs = plt.subplots(row,col)
+    fig.subplots_adjust(wspace=0.05, hspace=0.05)
+    axs = axs.flatten()
+    
+    for i in range(len(new_center)):
+        
+        percentage = 100*list2[i]/np.sum(list2)
+        
+        axs[i].set_axis_off()
+        axs[i].text(0,0,f'{percentage:.1f}%  {np.array(list1[i], dtype=int)}',
+          bbox={'facecolor':'white','alpha':1,'edgecolor':'none','pad':1},
+          ha='center', va='center', fontsize=10)
+        axs[i].imshow([[np.array(list1[i], dtype=int)]], aspect='auto')
+        
+    return fig
+
+    
+    # print(tmp)
+    # print(tmp2)
+    
+    # fig, ax = plt.subplots(1,1)
+    # for idx, (k,v) in enumerate(zip(centroids,counts)):    
+    #     ax.hist([idx]*round(100*v/(np.sum(counts))), color=k/255, rwidth=19)
+    # # plt.xticks(np.arange(len(counts)))
+    # return fig
+    
+def plot_result(centroids, array, vecs, shape):
+    # plt.clf()
+    
+    
+    for idx, color in enumerate(centroids):
+        fig, ax = plt.subplots(1,1)
+        ax.set_axis_off()
+    
+        res = np.ones_like(array)
+        res[scipy.r_[np.where(vecs==idx)],:] = color
+        res = np.array(res.reshape(*shape), dtype=int) 
+        ax.imshow(res)
+        # plt.show()
+        # plt.pause(0.5)
+
+
+def main():
+    # img_path = r'C:\Users\ipx\Desktop\Personal\Python_GUI\data\img\test.png'
+    img_path = r'C:\Users\ipx\Desktop\Personal\Python_GUI\data\img\FLIR_09312.jpg'
+
+    img = read_image(img_path)
+    array, shape = preprocess(img)
+    centroids, wieghts, hist_counts, vecs = gaussian_mixture(array) 
+    hist_plot = plot_histogram(centroids, hist_counts)
+
+    plt.show()
+
+    plot_result(centroids, array, vecs, shape)
+
+
+def hashing():
+    # img_path = r'C:\Users\ipx\Desktop\Personal\Python_GUI\data\img\test.png'
+    # img_path = r'C:\Users\ipx\Desktop\Personal\Python_GUI\data\img\FLIR_09312.jpg'
+
+    def dhash(image, hash_size=8):
+        resized = cv2.resize(image, (hash_size+1,hash_size))
+        
+        diff = resized[:,1:] > resized[:,:-1]
+        
+        return sum([2 ** i for (i, v) in enumerate(diff.flatten()) if v])
+
+
+    test_hash = 629407987240849968
+
+    database = {}
+
+    img_dir = r'C:\Users\ipx\Desktop\Personal\Python_GUI\data\img'
+
+    import glob, os
+    for img in glob.glob(os.path.join(img_dir, "*.jpg")):
+
+        image = cv2.imread(img)
+
+        if image is None:
+            continue        
+        
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        imageHash = dhash(image)
+        
+        path = database.get(imageHash, [])
+        
+
+        path.append(img)
+        database[imageHash] = path
+
+        print(path)
+
+    from scipy.spatial import distance
+
+    for k, v in database.items():
+        
+        dist = distance.hamming(test_hash, k)
+        
+        
+        print(dist)
+        
+        print(k)
+
+    
+    # hash = dhash(img)
+    
+    # print(hash)
+    
+    
+    # plt.imshow(img, cmap="gray")
+    # plt.show()
+
+
+
+if __name__ == "__main__":
+    # main()    
+    hashing()
